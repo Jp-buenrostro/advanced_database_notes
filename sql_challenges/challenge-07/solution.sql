5815e9ddaa65f2279c9f4c7176502c3a023d81de
Jp-buenrostro <141791220+Jp-buenrostro@users.noreply.github.com>
2026-04-29 13:33:28 -0600

    Session7

---
+++ sql_challenges/challenge-07/solution.sql
@@ -0,0 +1,20 @@
+-- 1 What is the UEFA Champions League 2025-26?
+
+CHUNK_ID    PREVIEW                                                                                SIMILARITY_SCORE
+1           La Liga de Campeones de la UEFA 2025-26 es la 71.ª edición del principal torneo...      0.2417
+3           Participaron 82 clubes europeos. El ganador de la Liga de Campeones de la UEFA...       0.3284
+2           La final se jugará el 30 de mayo de 2026 en el Puskás Aréna de Budapest, Hungría.       0.3521
+
+
+-- 2 Where will the final be played?
+
+CHUNK_ID    PREVIEW                                                                                SIMILARITY_SCORE
+2           La final se jugará el 30 de mayo de 2026 en el Puskás Aréna de Budapest, Hungría.       0.1985
+8           En julio de 2023, la UEFA anunció que Budapest había expresado interés en albergar...   0.3669
+9           La sede elegida para la final será el Puskás Aréna, ubicado en Budapest.                0.3924
+
+-- 3 What is Formula 1?
+CHUNK_ID    PREVIEW                                                                                SIMILARITY_SCORE
+7           Los equipos se clasifican según el rendimiento de sus ligas nacionales...              0.7684
+12          La competición incluye clubes de diferentes asociaciones europeas...                   0.7921
+14          El Paris Saint-Germain es el campeón defensor actual.                                 0.8157
